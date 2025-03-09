3bb308d57fd6c29fa73e4ed1ee1182f5c0964eed
DavidKopal <112717418+DavidKopal@users.noreply.github.com>
2025-03-09 15:43:52 +0100

    Update fixw.js

--- addons/fixw.js
+++ addons/fixw.js
@@ -2,4 +2,10 @@ extraWidth = () => {
   width = 150
   document.getElementById('game').width = 1500
   xWidth = 1
+  for (let x = 0; x < width; x++) {
+    if (!game[x]) game[x] = [];
+    for (let y = 0; y < 60; y++) {
+        if (!game[x][y]) game[x][y] = undefined;
+    }
+}
 }
