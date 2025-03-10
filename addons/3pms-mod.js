8000702d978b17ac128f6ca7dd4a708cc6220d59
3pm-on-github <pookiemon2019@gmail.com>
2025-03-10 18:54:18 +0100

    i somehow made red dust, anyways create 3pms-mod.js

---
+++ addons/3pms-mod.js
@@ -0,0 +1,14 @@
+// HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEELP HELP MEEEEEEEE HEEEEEEEEEEEEEEEEEEEEEEEEEEELP
+// mod by 3pm (github: 3pm-on-github)
+
+console.log("3pms mod exists")
+
+// note: red dust doesnt have any color change. why? im lazy rn
+bluestones.red_dust = {
+    color: "#FF0000",
+    description: "Like your everyday's wire but red.",
+    compression: "$rd",
+    ignore: ["green_dust", "dust"]
+}
+bluestones.green_dust.ignore = ["red_dust", "dust"]
+bluestones.dust.ignore = ["red_dust", "green_dust"]
